eb80878dedd4fa9adfd7fe0db607528f95ee9968
Matheus <matheushcastiglioni@gmail.com>
2017-01-02 20:42:40 -0200

    Finalizado recurso para popular select de acordo com cabeçalho de uma tabela

--- select.js
+++ select.js
@@ -4,23 +4,34 @@
  */
 document.addEventListener('DOMContentLoaded', function(event) {
 
+    const table = $('table.js-slTable');
+    if (table != undefined) {
+        const select = $('select[data-select=slTable]');
+        table.tHead.children[0].children.forEach(th => {
+           select.appendChild(new Option(th.textContent, th.getAttribute('scope')));
+        });
+        select.appendChild(new Option('Todos', ''));
+    }
+
     const selects = $$('select[data-select][data-url]');
-    selects.forEach(select => {
-        const url = getUrl(select.dataset.url);
-        HttpService.get('http://localhost:8080/mhcws/ws/' + url).then(response => {
-            let list = response.list;
-            let text;
-            let value;
-            switch (select.dataset.select) {
-                default:
-                    text = 'descricao';
-                    value = 'id'
-                    break; 
-            }
-            fillSelect(select, list, text, value);            
-            setOptipnSelected(select);
-        }).catch(error => console.log(error));        
-    });
+    if (selects.length > 0) {
+        selects.forEach(select => {
+            const url = getUrl(select.dataset.url);
+            HttpService.get('http://localhost:8080/mhcws/ws/' + url).then(response => {
+                let list = response.list;
+                let text;
+                let value;
+                switch (select.dataset.select) {
+                    default:
+                        text = 'descricao';
+                        value = 'id'
+                        break; 
+                }
+                fillSelect(select, list, text, value);            
+                setOptipnSelected(select);
+            }).catch(error => console.log(error));        
+        });
+    }
 
 });
 
