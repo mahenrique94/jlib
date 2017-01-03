0e323bd9058a8fafe27b17d2e22fb7e43712c8fd
Matheus <matheushcastiglioni@gmail.com>
2017-01-03 13:09:00 -0200

    Acerto para adicionar os valores min e max de cada input no tooltip

--- pattern.js
+++ pattern.js
@@ -7,9 +7,9 @@ document.addEventListener('DOMContentLoaded',function (event) {
 		{name : 'letraNumero', pattern : "^([A-Z\\d]+)$", message : "O campo deve ser preenchido com letras MAIUSCULAS ou numeros."},
 		{name : 'letraNumeroMin4', pattern : "^([A-Z\\d]{4,})$", message : "O campo deve ser preenchido com letras MAIUSCULAS ou numeros e deve ter no minimo 4 caracteres."},
 		{name : 'letraNumeroPonto', pattern : "^([A-Z\\d\\.]+)$", message : "O campo deve ser preenchido com letras MAIUSCULAS ou numeros e aceita os seguintes caracteres especiais: (.)."},
-		{name : 'numeric18-2', pattern : "^(([\\d]{1,18})(\\.([\\d]{1,2}))?)$", message : "O campo deve ser preenchido com letras numeros ou ponto, a parte inteira aceita no máximo 18 digitos e a decimal no máximo 2."},
-		{name : 'numeric18-4', pattern : "^(([\\d]{1,18})(\\.([\\d]{1,4}))?)$", message : "O campo deve ser preenchido com letras numeros ou ponto, a parte inteira aceita no máximo 18 digitos e a decimal no máximo 4."},
-		{name : 'numeric18-6', pattern : "^(([\\d]{1,18})(\\.([\\d]{1,6}))?)$", message : "O campo deve ser preenchido com letras numeros ou ponto, a parte inteira aceita no máximo 18 digitos e a decimal no máximo 6."},
+		{name : 'numeric18-2', pattern : "^(([\\d]{1,18})(\\.([\\d]{1,2}))?)$", message : "O campo deve ser preenchido com numeros ou ponto, a parte inteira aceita no maximo 18 digitos e a decimal no maximo 2."},
+		{name : 'numeric18-4', pattern : "^(([\\d]{1,18})(\\.([\\d]{1,4}))?)$", message : "O campo deve ser preenchido com numeros ou ponto, a parte inteira aceita no maximo 18 digitos e a decimal no maximo 4."},
+		{name : 'numeric18-6', pattern : "^(([\\d]{1,18})(\\.([\\d]{1,6}))?)$", message : "O campo deve ser preenchido com numeros ou ponto, a parte inteira aceita no maximo 18 digitos e a decimal no maximo 6."},
 		{name : 'password', pattern : "^([A-Z0-9@-_\\.]{8})$", message : "O campo deve ser preenchido com letras MAIUSCULAS ou numeros e aceita os seguintes caracteres especiais: (@ - _ .)."},
 		{name : 'data', pattern : '^(([0][1-9]|[1][0-9]|[2][0-9]|[3][0-1])\\/([0][1-9]|[1][0-2])\\/([19][7-9][0-9]+|[20][0-9]+))$', message : 'O campo deve preenchido com uma data no formato: DD/MM/AAAA onde o ano deve ser maior que 1900'},
 		{name : 'dataHora', pattern : '^(([0][1-9]|[1][0-9]|[2][0-9]|[3][0-1])\\/([0][1-9]|[1][0-2])\\/([19][0-9]+|[20][0-9]+)\\s([0][0-9]|[1][0-9]|[2][0-9]):([0][0-9]|[1][0-9]|[2][0-9]|[3][0-9]))$', message : 'O campo deve preenchido com uma data no formato: DD/MM/AAAA HH:MM onde o ano deve ser maior que 1900'}
@@ -20,11 +20,30 @@ document.addEventListener('DOMContentLoaded',function (event) {
 });
 
 function setPattern(pattern) {
-	let elements = $$(`input[pattern=${pattern.name}]`);
-	if (elements.length > 0) {
-		elements.forEach(element => {
-			element.pattern = pattern.pattern;
-			element.setAttribute('data-accept', pattern.message);
+	let inputs = $$(`input[pattern=${pattern.name}]`);
+	if (inputs.length > 0) {
+		inputs.forEach(input => {
+			input.pattern = pattern.pattern;
+			input.setAttribute('data-accept', pattern.message);
+			if (hasTooltip(input))
+				setMessageTooltip(pattern, input);
 		});
 	}
+}
+
+function hasTooltip(element) {
+	return element.parentNode.classList.contains('o-form__tooltip');
+}
+
+function setMessageTooltip(pattern, input) {
+	let tooltip = input.parentNode.querySelector('[class*=o-tooltip]');
+	tooltip.innerHTML = `${pattern.message}<br/>${buildMessageFromInput(input)}`;
+}
+
+function buildMessageFromInput(input) {
+	if (input.type.equals('text'))
+		return `Intervalo: ${input.getAttribute('minlength')} a ${input.getAttribute('maxlength')} caracteres.`;
+	if (input.type.equals('number'))
+		return `Intervalo: ${input.getAttribute('min')} a ${input.getAttribute('max')}.`;
+	return '';
 }
