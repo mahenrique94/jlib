9b68248829874b4fef47bb593d1ed479e5082c71
Matheus <matheushcastiglioni@gmail.com>
2017-01-27 15:10:19 -0200

    Adicionado arquivo para retirar auto complete dos inputs

---
+++ autocomplete.js
@@ -0,0 +1,10 @@
+/** @auth Matheus Castiglioni
+ *  Retirar auto complete dos formulários e inputs
+ */
+document.addEventListener('DOMContentLoaded', function(event) {
+	
+	const forms = $$('form');
+	if (forms.length > 0)
+		forms.forEach(form => form.setAttribute('autocomplete', 'off'));
+	
+});
